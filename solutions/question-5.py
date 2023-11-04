270b62f309f900e18b97bf63667066d67cfbe18b
harshshah-codes <149559646+harshshah-codes@users.noreply.github.com>
2023-11-04 13:09:38 +0530

    Added Solution-5

--- solutions/question-5.py
+++ solutions/question-5.py
@@ -1,7 +1,12 @@
 A = int(input())
 
 for i in range(A + 1):
+    flag = True
     for j in range(i + 1):
         if ((i * j) == A):
             print(j, i, sep=",")
+            flag = False
             break
+        
+    if not flag:
+        break
